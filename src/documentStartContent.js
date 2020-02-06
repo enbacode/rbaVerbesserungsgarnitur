8dfddccb2c8d856065ff8ab529692fbff4f2191b
enbacode <nick.baas94@gmail.com>
2020-02-06 03:24:19 +0100

    Fix active/inactive not considered when applying styles

--- src/documentStartContent.js
+++ src/documentStartContent.js
@@ -1,16 +1,21 @@
 import browser from 'webextension-polyfill'
 
 (async () => {
-    browser.storage.local.get('mods')
-    .then(mods => mods.mods)
-    .then(mods => {
-        let styles = []
-        mods.forEach(mod => {
-            if(mod.enabled && mod.active && ((mod.match && window.location.href.match(mod.match) || !mod.match)))
-                styles.push(mod.styleString)
-        } )
-        var style = document.createElement('style')
-        style.innerHTML = styles.join('\n').replace(/([a-z\-]+: .*);/gm, "$1 !important;")
-        document.documentElement.insertBefore(style, document.documentElement.firstChild);
+    browser.storage.local.get('active')
+    .then(active => {
+        if(!active.active)
+            return
+        browser.storage.local.get('mods')
+        .then(mods => mods.mods)
+        .then(mods => {
+            let styles = []
+            mods.forEach(mod => {
+                if(mod.enabled && mod.active && ((mod.match && window.location.href.match(mod.match) || !mod.match)))
+                    styles.push(mod.styleString)
+            } )
+            var style = document.createElement('style')
+            style.innerHTML = styles.join('\n').replace(/([a-z\-]+: .*);/gm, "$1 !important;")
+            document.documentElement.insertBefore(style, document.documentElement.firstChild);
+        })
     })
 })()
