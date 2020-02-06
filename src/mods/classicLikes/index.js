7053a7642afa7b299a926aa449f5ae7d4449834f
enbacode <nick.baas94@gmail.com>
2020-02-06 16:28:54 +0100

    Fix classicLikes Button in FF not working

--- src/mods/classicLikes/index.js
+++ src/mods/classicLikes/index.js
@@ -13,17 +13,15 @@ export default {
 
         $(document).ready(() => {
             $('.wbbPost.message').each((i, e) => {
-
                 const objectID = e.getAttribute('data-object-id')
                 let likeCount = $(e).find('.reactCountButton[data-reaction-type-id="2"] .reactionCount').text()
                 const likeText = $(e).find('.reactionSummaryList')
-                likeText.css("display", "block")
+                likeText.css('display', 'block')
                 likeText.empty()
                 if (likeCount)
                     likeText.html(`Es haben sich bereits ${likeCount} Benutzer bedankt`)
 
                 $(e).find('.reactButton').text('Bedanken').click((event) => {
-                    
                     let formData = `actionName=react&className=%5Cwcf%5Cdata%5Creaction%5CReactionAction&parameters%5Bdata%5D%5BobjectID%5D=${objectID}&parameters%5Bdata%5D%5BobjectType%5D=com.woltlab.wbb.likeablePost&parameters%5BreactionTypeID%5D=2`
 
                     ajax.post('https://rbaforum.de/index.php?ajax-proxy&t=' + ajax.getSecurityToken(), formData)
