872d251e46c2ca4597383db7d2b3651d940bc1c9
enbacode <nick.baas94@gmail.com>
2020-02-08 12:43:43 +0100

    use cascading configs

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,89 +2,21 @@ const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const ZipPlugin = require('zip-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const merge = require('webpack-merge')
 
-module.exports = {
+const fs = require('fs')
+
+const baseConfig = require('./webpack.config.js')
+
+module.exports = merge(baseConfig, {
     mode: 'production',
     optimization: {
         minimize: false
     },
-    entry: {
-        documentEndContent: './src/documentEndContent.js',
-        background: './src/background.js',
-        options: './src/options.js',
-        popup: './src/popup.js',
-        documentStartContent: './src/documentStartContent.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                          sassOptions: {
-                            indentWidth: 4,
-                            outputStyle: 'expanded',
-                            includePaths: ['absolute/path/a', 'absolute/path/b'],
-                          },
-                        },
-                      },
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.html$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                    },
-                }],
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                use: [{
-                    loader: 'url-loader',
-                }],
-            },
-            {
-                test: /\.json$/,
-                loader: 'file-loader',
-                type: 'javascript/auto',
-                options: {
-                    name: '[name].[ext]'
-                }
-            },
-            {
-                test: /\.txt$/i,
-                use: 'html-loader',
-            },
-        ],
-    },
     plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new VueLoaderPlugin(),
-        new CopyPlugin([
-            { from: 'static', to: '' },
-        ]),
         new ZipPlugin({
             path: '../zip',
-            filename: 'rbavg'
+            filename: `rbavg_latest.zip`
         })
-    ],
-}
+    ]
+})
